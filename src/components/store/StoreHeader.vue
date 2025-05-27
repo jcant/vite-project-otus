d0f5f0f6e0e473781682a5d3ccded7b635008100
Anton Isaev <jcant.gm@gmail.com>
2025-05-27 11:12:16 +0300

    change project file structure

---
+++ src/components/store/StoreHeader.vue
@@ -0,0 +1,21 @@
+<script setup>
+import NavigationMenu from "./NavigationMenu.vue";
+import ProductFilter from "./ProductFilter.vue";
+
+const props = defineProps(["products"]);
+const emit = defineEmits(["filter-products", "clear-filter"]);
+</script>
+
+<template>
+  <div class="grid grid-cols-[70%_30%] bg-amber-400 p-4 place-items-center">
+    <div>STORE NAME</div>
+    <div><NavigationMenu /></div>
+    <ProductFilter
+      :products="products"
+      @filter-products="emit('filter-products', $event)"
+      @clear-filter="$emit('clear-filter')"
+    />
+  </div>
+</template>
+
+<style scoped></style>
